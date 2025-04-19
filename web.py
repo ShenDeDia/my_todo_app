44f80e6f907c371ddf7593df5ab37f8d40e6ce04
Miku <karimovamal838@gmail.com>
2025-04-19 21:17:43 +0500

    Initial commit

--- web.py
+++ web.py
@@ -13,11 +13,11 @@ st.subheader("this is my todo app")
 st.write("this web increase your productivity")
 
 for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=todo)
+    checkbox = st.checkbox(todo, key=f"{todo}_{index}")
     if checkbox:
         todos.pop(index)
         functions.write_todo(todos)
-        del st.session_state[todo]
+        del st.session_state[f"{todo}_{index}"]
         st.rerun()
 
 st.text_input(label="Enter to-do:",
